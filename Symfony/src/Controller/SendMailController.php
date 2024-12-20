6cee02d2f89bc05389b242ce7253d45d185ae827
MaheryJeremie <mramahay2103@gmail.com>
2024-12-20 10:40:27 +0300

    modif kely

--- Symfony/src/Controller/SendMailController.php
+++ Symfony/src/Controller/SendMailController.php
@@ -110,33 +110,40 @@ class SendMailController extends AbstractController
         SessionInterface $session
     ): Response {
         $data = json_decode($request->getContent(), true);
-
+    
+        // Validation des champs requis
         if (!isset($data['username'], $data['email'], $data['password'], $data['birthDate'], $data['genderId'])) {
             return $this->json(['message' => 'Missing required fields'], 400);
         }
-
+    
+        // Validation de l'email
         if (empty($data['email']) || !filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
             return $this->json(['message' => 'Invalid or missing email address'], 400);
         }
-
+    
+        // Vérification si l'email est un compte Gmail
+        if (!str_ends_with($data['email'], '@gmail.com')) {
+            return $this->json(['message' => 'L\'adresse email doit être un compte Google Mail (@gmail.com).'], 400);
+        }
+    
         $user = new Users();
         $user->setUsername($data['username']);
         $user->setEmail($data['email']);
         $user->setPassword($data['password']);
         $user->setBirthDate(new \DateTime($data['birthDate']));
-
+    
         $roleId = isset($data['roleId']) ? $data['roleId'] : null;
         $role = $roleId ? $this->roleRepository->find($roleId) : $this->roleRepository->find(1);
-
+    
         if (!$role) {
             $role = $this->roleRepository->find(1);
         }
-
+    
         $gender = $this->genderRepository->find($data['genderId']);
-
+    
         $user->setRole($role);
         $user->setGender($gender);
-
+    
         $session->set('user', [
             'username' => $user->getUsername(),
             'email' => $user->getEmail(),
@@ -145,18 +152,18 @@ class SendMailController extends AbstractController
             'role' => $user->getRole()->getRoleId(),
             'gender' => $user->getGender()->getGenderId(),
         ]);
-
+    
         $htmlContent = file_get_contents($this->getParameter('kernel.project_dir') . '/public/inscription.html');
-
+    
         $email = (new Email())
             ->from('projetclouds5p16@gmail.com')
             ->to($data['email'])
             ->subject('Bienvenue sur notre plateforme')
             ->html($htmlContent);
-
+    
         try {
             $mailer->send($email);
-
+    
             return $this->json([
                 'message' => "Email envoyé avec succès !",
                 'user' => $session->get('user')
