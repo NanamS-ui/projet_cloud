62757aa0a1379d7eb69e229864b4610fba34d971
MaheryJeremie <mramahay2103@gmail.com>
2024-12-20 13:16:59 +0300

    V1 modif farany

--- Symfony/src/Controller/LoginController.php
+++ Symfony/src/Controller/LoginController.php
@@ -134,13 +134,80 @@ class LoginController extends AbstractController
             ->from('projetclouds5p16@gmail.com')
             ->to($user->getEmail())
             ->subject('Connexion réussie')
-            ->html('<html>...</html>');
+            ->html('<html>
+                <head>
+                    <style>
+                        body {
+                            font-family: Arial, sans-serif;
+                            background-color: #f4f4f9;
+                            padding: 20px;
+                        }
+                        .container {
+                            background-color: #ffffff;
+                            border-radius: 8px;
+                            padding: 20px;
+                            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                            max-width: 600px;
+                            margin: auto;
+                        }
+                        h2 {
+                            color: #333;
+                        }
+                        h3 {
+                            color: #4CAF50;
+                        }
+                        p {
+                            font-size: 16px;
+                            color: #555;
+                        }
+                        .lien p {
+                            display: inline-block;
+                            padding: 10px 20px;
+                            background-color: #007BFF;
+                            color: #fff;
+                            text-decoration: none;
+                            border-radius: 5px;
+                            font-weight: bold;
+                            text-align: center;
+                        }
+
+                        .lien p:hover {
+                            background-color: #0056b3;
+                        }
+                        .footer {
+                            font-size: 14px;
+                            color: #777;
+                            text-align: center;
+                            margin-top: 20px;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <div class="container">
+                        <h2>Bonjour ' . $user->getUsername() . ',</h2>
+                        <p>Vous vous êtes connecté avec succès ! Votre code PIN est :</p>
+                        <h3>' . $pin . '</h3>
+                        <p>Le code PIN expirera dans 90 secondes.</p>
+                        <p>Voici url pour valider votre code PIN :</p>
+                         <p class="lien">
+                            <p>
+                                http://127.0.0.1:8000/api/verifyPin
+                            </p>
+                        </p>
+                        <div class="footer">
+                            <p>Merci de votre connexion. Si vous n êtes pas l origine de cette demande, veuillez nous contacter immédiatement.</p>
+                        </div>
+                    </div>
+                </body>
+                </html>');
 
         $mailer->send($emailMessage);
 
-        return $this->json([
+        $responseData = [
             'messageSuccess' => 'Connexion réussi, vérifiez vos messages dans votre boîte mail pour la confirmation'
-        ]);
+        ];
+
+        return $this->json($responseData);
     }
 
     #[Route('/api/verifyPin', name: 'verify_Pin', methods: ['POST'])]
@@ -166,6 +233,10 @@ class LoginController extends AbstractController
     #[OA\Response(response: 429, description: "Trop de tentatives de validation")]
     public function verifyPin(Request $request, SessionInterface $session, MailerInterface $mailer, DureeSessionRepository $dureeSessionRepository, LimiteTentativeConnectionRepository $limiteTentativeConnectionRepository): JsonResponse
     {
+        $duree = $dureeSessionRepository->findAll()[0];
+        ini_set('session.gc_maxlifetime', $duree->getDuree());
+        session_set_cookie_params($duree->getDuree());
+
         $data = json_decode($request->getContent(), true);
 
         if (!isset($data['email'], $data['pin'])) {
@@ -177,7 +248,9 @@ class LoginController extends AbstractController
             return $this->json(['message' => 'Email not found'], 400);
         }
 
-        $attemptsKey = 'pin_attempts_' . $data['email'];
+        $email = $data['email'];
+
+        $attemptsKey = 'pin_attempts_' . $email;
         $attemps = $session->get($attemptsKey, 0);
 
         $limit = $limiteTentativeConnectionRepository->findAll()[0];
@@ -197,7 +270,7 @@ class LoginController extends AbstractController
             $emailMessage = (new Email())
                 ->from('projetclouds5p16@gmail.com')
                 ->to($data['email'])
-                ->subject('Réinitialisation des tentatives de validation du PIN')
+                ->subject('Réinitialisation des tentatives de connexion')
                 ->html($htmlContent);
 
             $mailer->send($emailMessage);
@@ -213,28 +286,61 @@ class LoginController extends AbstractController
 
         if ($generatePin == null) {
             $session->set($attemptsKey, $attemps + 1);
+            $session->set('email', $email);
             return $this->json([
-                'message' => 'Invalid PIN',
+                'message' => 'Invalidate pin',
                 'attempts' => $attemps + 1
             ], 400);
         }
 
         $session->remove($attemptsKey);
+        $session->remove('email');
 
-        // Générer un token pour l'utilisateur
         $user->setToken(Util::generateToken());
         $this->entityManager->flush();
 
         $session->set('user', $user->getUserId());
 
-        return $this->json([
-            'messageSuccess' => 'Connexion réussie, token créé avec succès',
+        $responseData = [
+            'messageSuccess' => 'Connexion successful, token created successfully',
             'id' => $user->getUserId(),
             'username' => $user->getUsername(),
             'email' => $user->getEmail(),
             'birthDate' => $user->getBirthDate()->format('Y-m-d'),
             'role' => $user->getRole()->getName(),
             'gender' => $user->getGender()->getName()
+        ];
+
+        return $this->json($responseData);
+    }
+
+    #[Route('/api/reinitialiser', name: 'reinitialiser', methods: ['POST'])]
+    public function resetAttempts(Request $request, SessionInterface $session): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $email = $data['email'] ?? $session->get('email');
+
+        if (!$email) {
+            return $this->json([
+                'message' => 'Email is required to reset login attempts.'
+            ], 400);
+        }
+
+        $loginAttemptsKey = 'login_attempts_' . $email;
+        $pinAttemptsKey = 'pin_attempts_' . $email;
+
+        if ($session->has($loginAttemptsKey)) {
+            $session->remove($loginAttemptsKey);
+        }
+
+        if ($session->has($pinAttemptsKey)) {
+            $session->remove($pinAttemptsKey);
+        }
+
+        $session->remove('email');
+
+        return $this->json([
+            'message' => 'Login attempts and PIN attempts have been reset for ' . $email . '.'
         ]);
     }
 }
